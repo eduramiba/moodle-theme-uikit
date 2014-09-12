cba90ce865532d587c78dcc30fbf683f518b7c72
Eduardo Ramos <eduramiba@gmail.com>
2014-09-12 21:15:12 +0200

    Bump version number.

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2014090600;        // YYYYMMDDXX
+$plugin->version   = 2014091200;        // YYYYMMDDXX
 $plugin->maturity = MATURITY_STABLE;             // this version's maturity level.
-$plugin->release = '2.7 (Build: 2014090600)';
+$plugin->release = '2.7 (Build: 2014091200)';
 $plugin->requires  = 2013051401;        // Requires Moodle 2.5.1.
 $plugin->component = 'theme_uikit';
