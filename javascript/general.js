cde36139ed59c50fad796ecfde1ec1700692afc1
Eduardo Ramos <eduramiba@gmail.com>
2015-02-17 01:14:07 +0100

    Remove navigation accordion completely

--- javascript/general.js
+++ javascript/general.js
@@ -15,22 +15,5 @@
                 $('#toTop').fadeOut();
             }
         });
-
-        $("#navigationAccordionToggle").click(function(event) {
-            event.preventDefault();
-            $("#navigationAccordion").slideToggle();
-        });
-        
-        $(window).resize(function() {
-            if(this.resizeTO) clearTimeout(this.resizeTO);
-            this.resizeTO = setTimeout(function() {
-                $(this).trigger('resizeEnd');
-            }, 300);
-        });
-        
-        //Restore accordion visibility on window resize:
-        $(window).bind('resizeEnd', function() {
-            $("#navigationAccordion").attr('style', null);//Remove slideToggle display rule
-        });
     });
 })(jQuery);
