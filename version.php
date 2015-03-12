4e4510a66f80861ae631a7dd3d096e63f5c071c7
Eduardo Ramos <eduramiba@gmail.com>
2015-03-12 20:22:11 +0100

    Fix Issue #22 - Background image getting cut off

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2015021901;        // YYYYMMDDXX
+$plugin->version   = 2015031200;        // YYYYMMDDXX
 $plugin->maturity = MATURITY_STABLE;             // this version's maturity level.
-$plugin->release = '2.8 (Build: 2015021901)';
+$plugin->release = '2.8 (Build: 2015031200)';
 $plugin->requires  = 2013051401;        // Requires Moodle 2.5.1.
 $plugin->component = 'theme_uikit';
