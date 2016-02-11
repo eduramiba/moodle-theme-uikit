40407d323bd42911975742b7107576afa9cc892f
Eduardo Ramos <eduramiba@gmail.com>
2016-02-11 20:57:24 +0100

    Fix #44 Collapsed block items appear uncollapsed in Moodle 2.8+

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2016021100;        // YYYYMMDDXX
+$plugin->version   = 2016021101;        // YYYYMMDDXX
 $plugin->maturity = MATURITY_STABLE;             // this version's maturity level.
-$plugin->release = '3.0 (Build: 2016021100)';
+$plugin->release = '3.0 (Build: 2016021101)';
 $plugin->requires  = 2013051401;        // Requires Moodle 2.5.1.
 $plugin->component = 'theme_uikit';
